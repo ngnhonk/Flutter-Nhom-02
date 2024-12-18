3be5b6ffd8c1d332e2de2f3a5c1bc3befbc5e81f
Viet Hung <121851963+ngnhonk@users.noreply.github.com>
2024-12-18 10:22:37 +0000

    update

---
+++ ProjectName/khongcoten/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/workspaces/Nhom-02/ProjectName/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/workspaces/Nhom-02/ProjectName/khongcoten" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/workspaces/Nhom-02/ProjectName/flutter"
+  "PROJECT_DIR=/workspaces/Nhom-02/ProjectName/khongcoten"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/workspaces/Nhom-02/ProjectName/khongcoten/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/workspaces/Nhom-02/ProjectName/khongcoten/lib/main.dart"
+)
