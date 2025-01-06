aba1213216b9b5982d5a6cf9f0a4370430e1babd
Honk <hungnguyenviet204@gmail.com>
2025-01-06 14:39:18 +0700

    fix file

---
+++ app/lib/DETAILS-SCREEN-README.md
@@ -0,0 +1,31 @@
+## Về các phần code:
+
+### Class User:
+
+1. Định nghĩa class User theo yêu cầu của đề bài bao gồm username, password và role.
+
+2. Khởi tạo mảng các User thông qua "final List<User> users = ..."
+
+### Class ForumDetailScreen:
+
+1. Class ForumDetailScreen extends StatelessWidge: để tạo layout cho phần hiển thị thông tin của các user đã khởi tạo với GridView. Trong đó phần thông số của GridView:
+   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+   crossAxisCount: 2, // điều chỉnh cột
+   crossAxisSpacing: 8.0, // kc cột dọc
+   mainAxisSpacing: 8.0, // kc cột ngang
+   childAspectRatio: 3 / 2, // tỉ lệ 3/2 cho thẻ child
+   ),
+
+2. Phần thông tin để hiển thị các thông tin của object đã tạo:
+   Text(
+   'Username: ${user.username}',
+   style: const TextStyle(
+   fontWeight: FontWeight.bold,
+   ),
+   ),
+   Text('Role: ${user.role}'),
+
+- Tại đây có thể truy cập được tên của object thông qua các dot notation như bình thường.
+
+### main.dart:
+1. Class HomeScreen trong main.dart ban đầu được xây dựng với ý tưởng để gọi tới các ForumDetailScreen với ID tăng dần. Nhưng trong phần này em đã sửa đổi itemCount = 1 để chỉ hiển thị ra 1 phần duy nhất. tại trang chủ khi bấm vào "Cửa sổ hiển thị User 1" thì sẽ dẫn tới phần view của class ForumDetailScreen.
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:app/details_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class User {
-  String username;
-  String password;
-  String role;
-
-  User(this.username, this.password, this.role);
-}
-
-final List<User> users = [
-  User('Nguyen Viet Hung', 'pass1', 'Admin'),
-  User('user2', 'pass2', 'User'),
-  User('user3', 'pass3', 'User'),
-  User('user4', 'pass4', 'User'),
-  User('user5', 'pass5', 'Guest'),
-];
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -315,56 +301,3 @@ class NewsDetailScreen extends StatelessWidget {
   }
 }
 
-class ForumDetailScreen extends StatelessWidget {
-  final int forumId;
-
-  const ForumDetailScreen({super.key, required this.forumId});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('GridView hiển thị User $forumId:'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 8.0,
-            mainAxisSpacing: 8.0,
-            childAspectRatio: 3 / 2,
-          ),
-          itemCount: users.length,
-          itemBuilder: (context, index) {
-            final user = users[index];
-            return Card(
-              elevation: 4.0,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.account_circle,
-                      size: 40,
-                      color: Colors.blueAccent,
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Username: ${user.username}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text('Role: ${user.role}'),
-                  ],
-                ),
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
